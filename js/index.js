792e981aa0b97d8ab956fe6971a0a3c471ece668
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-03-18 11:27:05 +0100

    Spotify suport **for alarms that are triggered 2min after program start**

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ const remote = require('electron').remote
 const dialogs = require('dialogs')()
 const notifier = require('node-notifier')
 const path = require('path')
+const SpotifyWebHelper = require('spotify-web-helper')
+const helper = SpotifyWebHelper()
 
 // global objects
 const shutdownTimer = new ShutdownTimer()
@@ -87,6 +89,9 @@ window.onload = () => {
     // rotate image
     rotatingImage.classList.add('state-rotate')
 
+    // stop music
+    helper.player.pause()
+
     // start timeout (20s) for forcefully shutting down the computer
     const shutdownTimeout = setTimeout(() => {
       // simple system shutdown with default options
@@ -99,7 +104,11 @@ window.onload = () => {
     dialogs.confirm(
       'Stop the computer from shutting down? (in 20s this will automatically happen)',
       okWasPressed => {
-        if (!okWasPressed) return
+        if (!okWasPressed) {
+          // play music again
+          helper.player.play()
+          return
+        }
         clearTimeout(shutdownTimeout)
         rotatingImage.classList.remove('state-rotate')
       }
@@ -107,7 +116,7 @@ window.onload = () => {
 
     notifier.notify(
       {
-        title: 'Timer is finished (' + (t.msInput / 1000 / 60) + 'min)',
+        title: 'Timer is finished (' + t.msInput / 1000 / 60 + 'min)',
         message: 'The computer is about to shut down (20s) - click here to stop this from happening!',
         icon: path.join(__dirname, 'icon/icon.png'),
         sound: true,
@@ -123,11 +132,14 @@ window.onload = () => {
         // clear timeout / stop shutdown
         clearTimeout(shutdownTimeout)
 
+        // play music again
+        helper.player.play()
+
         // rotate image
         rotatingImage.classList.remove('state-rotate')
 
         // restore window if it's minimized
-        if (remote.getCurrentWindow().isMinimized()) remote.getCurrentWindow().restore()
+        if (remote.getCurrentWindow().isMinimized()) { remote.getCurrentWindow().restore() }
         // focus the window
         remote.getCurrentWindow().focus()
       }
@@ -141,7 +153,15 @@ window.onload = () => {
     // set time display content
     if (t.d !== 0) {
       timeDisplay.textContent =
-        pad(t.d, 2) + ':' + pad(t.h, 2) + ':' + pad(t.m, 2) + ':' + pad(t.s, 2) + ':' + pad(t.ms, 3)
+        pad(t.d, 2) +
+        ':' +
+        pad(t.h, 2) +
+        ':' +
+        pad(t.m, 2) +
+        ':' +
+        pad(t.s, 2) +
+        ':' +
+        pad(t.ms, 3)
     } else if (t.h !== 0) {
       timeDisplay.textContent =
         pad(t.h, 2) + ':' + pad(t.m, 2) + ':' + pad(t.s, 2) + ':' + pad(t.ms, 3)
@@ -210,11 +230,6 @@ window.onload = () => {
       case 116: // F5
         remote.getCurrentWindow().reload()
         break
-      case 122: // F11
-        remote
-          .getCurrentWindow()
-          .setFullScreen(!remote.getCurrentWindow().isFullScreen())
-        break
       case 123: // F12
         remote.getCurrentWindow().toggleDevTools()
     }
@@ -226,6 +241,12 @@ window.onload = () => {
       case 32: // space bar
         if (shutdownTimer.isPaused) shutdownTimer.resume()
         else shutdownTimer.pause()
+        break
+      case 122: // F11
+        remote
+          .getCurrentWindow()
+          .setFullScreen(!remote.getCurrentWindow().isFullScreen())
+        break
     }
   })
 
@@ -235,5 +256,45 @@ window.onload = () => {
   })
   remote.getCurrentWindow().on('leave-full-screen', () => {
     electronTitlebar.style.display = 'block'
+  })
+
+  console.log(helper)
+  console.log(helper.player)
+  console.log(helper.player.pause())
+
+  const spotifyWebhelperStarted = window.performance.now()
+
+  function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000)
+    var seconds = ((millis % 60000) / 1000).toFixed(0)
+    return minutes + 'min ' + (seconds < 10 ? '0' : '') + seconds + 's'
+  }
+
+  helper.player.on('error', err => {
+    console.error('Spotify web helper error', err)
+    if (error.message.match(/No user logged in/)) {
+      console.error('No user logged in or spotify was closed')
+    } else {
+      console.error('Spotify not installed or another error')
+    }
+  })
+  helper.player.on('ready', () => {
+    // dialog to inform spotify helper is ready
+    dialogs.alert('Spotify helper is ready (' + millisToMinutesAndSeconds(window.performance.now() - spotifyWebhelperStarted) + ')')
+    // show notification to inform that spotify helper is ready
+    notifier.notify(
+      {
+        title: 'Spotify web helper is ready',
+        message: 'Whohooooo (' + millisToMinutesAndSeconds(window.performance.now() - spotifyWebhelperStarted) + ')',
+        icon: path.join(__dirname, 'icon/icon.png'),
+        sound: true,
+        wait: true
+      },
+      (err, response) => {
+        if (err) console.error(err)
+        // close open dialogs - not important if the user clicked
+        dialogs.cancel()
+      }
+    )
   })
 }
